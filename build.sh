13053836bac80b54dc859a671c306c6dc2dd6a2f
alysonjan <alysonjan@gmail.com>
2025-05-08 23:36:27 +0800

    Add build.sh to repository

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+echo "Clearing node_modules and package-lock.json"
+rm -rf node_modules package-lock.json
+echo "Running npm install"
+npm install --verbose
+echo "Running npm run build"
+npm run build
